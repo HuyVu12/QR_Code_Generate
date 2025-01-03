3d256ae2fdd5d57160aa68bac6e40ce5c2b70a41
HuyVu12 <64604579+HuyVu12@users.noreply.github.com>
2025-01-03 13:59:01 +0700

    First init

---
+++ main.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import qrcode
+import uuid
+
+st.title("QR Code Generator")
+
+st.write("Enter some text for generating QR code:")
+text = st.text_input("Text")
+
+def gen_qr(text):
+    qrcode.make(text)
+    file_name = f"{uuid.uuid4()}.png"
+    qrcode.make(text).save(file_name)
+    print(f"QR code saved to {file_name}")
+    return file_name
+
+if text:
+    file_name = gen_qr(text)
+    st.image(file_name, caption=f"QR code for {text}")
