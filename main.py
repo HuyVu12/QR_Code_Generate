2b679afadfd41b0086ae761092899eeebf145b5f
HuyVu12 <64604579+HuyVu12@users.noreply.github.com>
2025-01-03 15:10:32 +0700

    update generate text, reset, change some UI

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import qrcode
 import uuid
+import lorem
+import os 
+
+if 'textInput' not in st.session_state:
+    st.session_state.textInput = ''
+
+if 'file_name' not in st.session_state:
+    st.session_state.file_name = ''
 
 st.title("QR Code Generator")
-
-st.write("Enter some text for generating QR code:")
-text = st.text_area("Text")
 
 def gen_qr(text):
     qrcode.make(text)
@@ -14,6 +19,34 @@ def gen_qr(text):
     print(f"QR code saved to {file_name}")
     return file_name
 
-if st.button('Generate'):
-    file_name = gen_qr(text)
-    st.image(file_name, caption=f"{text}")
+m_col1, m_col2 = st.columns([2, 1], vertical_alignment='center')
+
+with m_col1:    
+    text = st.text_area("Text", 
+                        placeholder="Enter text here for generating QR code",
+                        value=st.session_state.textInput)
+    
+    st.session_state.textInput = text
+    m_col1_1, m_col1_2 = st.columns([1, 1], vertical_alignment='center')
+    with m_col1_1:
+        if st.button('Generate'):
+            st.session_state.file_name = gen_qr(text)
+    with m_col1_2:
+        if st.button('Reset'):
+            st.session_state.textInput = ''
+            st.rerun()
+
+    col1, col2 = st.columns(2, vertical_alignment='bottom')
+    with col1:
+        st.number_input(min_value=1, value=1, key="num_gen_lorem", label="Number paragraph")
+    with col2:
+        if st.button('Generate Some Text'):
+            text = ''
+            for _ in range(st.session_state.num_gen_lorem):
+                text += lorem.paragraph()
+            st.session_state.textInput = text
+            st.rerun()
+
+with m_col2:
+    if len(st.session_state.file_name) > 0:
+        st.image(st.session_state.file_name, caption=f"{os.getcwd()}/{st.session_state.file_name}")
